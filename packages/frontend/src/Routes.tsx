51df2cf6e1c10b27419669e8ae692d7ec8059acf
nadyrbek97 <nadyrbek97@gmail.com>
2024-07-07 21:14:41 +0600

    sign up page

--- packages/frontend/src/Routes.tsx
+++ packages/frontend/src/Routes.tsx
@@ -1,6 +1,8 @@
 import { Route, Routes } from "react-router-dom";
 import Home from "./containers/Home.tsx";
 import NotFound from "./containers/NotFound.tsx";
+import Login from "./containers/Login.tsx";
+import Signup from "./containers/Signup.tsx";
 
 
 export default function Links() {
@@ -9,6 +11,8 @@ export default function Links() {
       <Route path="/" element={<Home />} />
       {/* Finally, catch all unmatched routes */}
       <Route path="*" element={<NotFound />} />;
+      <Route path="/login" element={<Login />} />;
+      <Route path="/signup" element={<Signup />} />
     </Routes>
   );
 }
